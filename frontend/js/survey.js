20fa3a0e1a45e2b697e7cae567f527adf03d7aa0
Andrew Vittiglio <andrew@andrews-air.dhcp.nd.edu>
2023-02-27 04:06:58 -0500

    New homepage

---
+++ frontend/js/survey.js
@@ -0,0 +1,43 @@
+/*
+
+**********Data that we want to know about the user***********
+
+Logitsics:
+1. Trip Departure Date
+2. Trip Return Date
+3. Number of Adult Travellers
+4. Origin City
+5. Budget - via slider
+
+Passenger Details:
+6. First Name
+7. Last Name
+8. Gender 
+9. DOB 
+10. Country of Residence + State of Residence
+11. Phone Number
+12. Primary Email
+
+Payment Information: 
+13. First and Last Name on Card
+14. Card Number
+15. Expiration Date
+16. CVV
+17. Billing Address, City, State, Zip Code
+18. Card Type (Visa, Mastercard, etc.)
+19. Country/Region of Card Issuance
+
+Personal Question for AI (less is more for this): 
+1. Countries you have visited before
+2. Preferred Level of Activity - Adventure or Sightseeing or Relaxing
+3. What is the purpose of your trip? Are you traveling for leisure, business, or a special occasion such as a honeymoon or anniversary? This will help me suggest destinations that align with the purpose of your trip.
+4. Do you have any special requirements or considerations? Do you have any mobility issues, dietary restrictions, or other special requirements that I should be aware of? This will help me recommend destinations that are accommodating to your needs.
+
+Optionals:
+1. Region Preference
+2. Non-Stop Flights Only 
+3. Add on Bags
+4. Exclude Certain Airlines
+5. Cabin Preference (Defaults to cheapest)
+
+*/
