4159cdfa99a7a855c65c2a5bd5db8025b269ed77
Andrew Vittiglio <andrew@andrews-air.dhcp.nd.edu>
2023-03-31 02:29:40 -0400

    new GPT-4 itinerary

--- model/itinerary.py
+++ model/itinerary.py
@@ -2,43 +2,58 @@
 
 import os
 import openai
-import asyncio
+import json
 from dotenv import load_dotenv
-from EdgeGPT import Chatbot
+
+load_dotenv() # Load environment variables from .env file
+openai.api_key = os.getenv('OPEN_AI_KEY')
+
+#open json file
+
+# Open the JSON file for reading
+with open('data/jotform.json', 'r') as file:
+    # Load the contents of the file into a variable
+    jotform = json.load(file)
+
+query = {
+	'first': jotform['4']['answer']['first'],
+	'birthday': jotform['65']['prettyFormat'],
+    'budget': int(jotform['56']['answer']),
+    'visited': jotform['58']['answer'],
+    'fly_from': jotform["51"]["answer"],
+    'date_from': jotform["53"]["answer"]["month"]+ ' ' + jotform["53"]["answer"]["day"],
+	'date_to': jotform["54"]["answer"]["month"]+ ' ' + jotform["54"]["answer"]["day"],
+    'numPassengers': int(jotform['50']['answer']),
+    'gender': jotform['3']['answer']
+
+}
 
 
-def getCountry():
-	load_dotenv() # Load environment variables from .env file
-	api_key = os.getenv('OPEN_AI_KEY') # Get the value of APIKEY1 from environment variables
-	openai.api_key = api_key #set API key
+system_intel = 'You are a experienced travel agent. You are going to recommend one travel destination for a client based on their profile. The client wants to travel spontaneously and wants you to recommend a random destination. Make sure the destination you choose is feasible given the client’s budget and availability. For example, if a client only has 5 days to travel, you wouldn’t want to recommend a destination in Asia because the client will need more time to visit most countries in Asia. DO NOT recommend anywhere in Mexico, Canada, the United Kingdom, the Caribbean. DO NOT recommend any of the countries the client has already been to. DO NOT recommend the most visited mainstream European cities. DO recommend destinations that the average US traveler hasn’t been to. For destination recommendation, only output the country name, nothing else.  For itinerary generation, make each day sound exciting!'
+prompt = f"Your client is {query['first']}. {query['first']} is a {query['gender']} from the United States of America. He is going to be traveling with {query['numPassengers']} other person and they are flying out of {query['fly_from']}. He is leaving on {query['date_from']} and returns on {query['date_to']}. The budget for the whole trip is {query['budget']}. They have been to these countries before: {query['visited']}. The reason they travels is because they love the feeling of exploring a new place. Where should they go?"
+prompt2 = "Create an itinerary!"
+messages = [
+    	{"role": "system", "content": system_intel}, 
+    	{"role": "user", "content": prompt}]
 
-	response = openai.Completion.create(
-	model="text-davinci-003",
-	prompt="Act as if you are a travel agent. You are going to recommend one travel destination for a client based on their profile. The client wants to travel spontaneously and wants you to recommend a semi-random destination. Make sure the destination you choose is feasible given the client’s budget and availability. For example, if a client only has 5 days to travel, you wouldn’t want to recommend a destination in Asia because the client will need more time to visit most countries in Asia. The destination can be a country, city, region, etc. depending on if it fits the client’s timeline.  Do not recommend anywhere in Mexico, Canada, the United Kingdom, the Caribbean, or any of the countries the client has already been to; you want to recommend destinations that the average US traveler hasn’t been to. Also try to avoid most mainstream European cities. Only output the country name, nothing else. CLIENT INFO: Your client is Andrew. Andrew is a 20 year old male from the United States of America. He is going to be traveling with one other person and they are flying out of Chicago. He is leaving on May 8th and wants to return on May 19th. His budget for the whole trip is $3000. He has been to these countries before: the United States, Costa Rica, France, Italy, Switzerland, Germany, Mexico, the Caribbean, Japan, and Spain. The reason he travels is because he loves the feeling of exploring a new place. ",
-	temperature=0.7,
-	max_tokens=50,
-	top_p=1,
-	frequency_penalty=0,
-	presence_penalty=0
-	)
-	print(response)
-	return response
+response = openai.ChatCompletion.create(
+	model="gpt-4",
+	max_tokens= 2048,
+	temperature=1,
+	messages=messages
+    )
 
-async def getItinerary(country):
-	itineraryMsg = f"In less than 1000 words, create an full itinerary for your client for the country of {country}. In the itinerary, estimate the costs. The client is Andrew. Andrew is a 20 year old male from the United States of America. He is going to be traveling with one other person and they are flying out of Chicago. He is leaving on May 8th and wants to return on May 19th. His budget for the whole trip is $3000. The reason he travels is because he loves the feeling of exploring a new place. Please keep it to less than 1000 words!"
+country = response['choices'][0]['message']['content']
+messages.append({"role": "assistant", "content": country})
+messages.append({"role": "user", "content": prompt2})
 
-	bot = Chatbot(cookiePath='./cookies.json')
-	response = await bot.ask(prompt = itineraryMsg)	
-	response2 = await bot.ask(prompt = "Continue")
-	response3 = await bot.ask(prompt = "Continue")
-	await bot.close()
+response = openai.ChatCompletion.create(
+	model="gpt-4",
+	max_tokens= 2048,
+	temperature=1,
+	messages=messages
+    )
+itinerary = response['choices'][0]['message']['content']
 
-	return [response["item"]["messages"][1]["text"], response2["item"]["messages"][1]["text"], response3["item"]["messages"][1]["text"]]
-
-async def main():
-	country = getCountry()
-	itinerary = await getItinerary(country)
-	print(itinerary)
-
-# Call the main function to start the event loop and run the coroutine
-asyncio.run(main())
+print(country)
+print(itinerary)
