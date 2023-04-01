5fd3629ed247b612994467c2ad4a01f4ec99aa71
Andrew Vittiglio <andrew@andrews-air.dhcp.nd.edu>
2023-04-01 16:24:02 -0400

    finished itinerary

--- model/itinerary.py
+++ model/itinerary.py
@@ -1,5 +1,3 @@
-#TODO: Change prompt to be dynamic with user input
-
 import os
 import openai
 import json
@@ -16,21 +14,19 @@ with open('data/jotform.json', 'r') as file:
     jotform = json.load(file)
 
 query = {
-	'first': jotform['4']['answer']['first'],
-	'birthday': jotform['65']['prettyFormat'],
-    'budget': int(jotform['56']['answer']),
-    'visited': jotform['58']['answer'],
-    'fly_from': jotform["51"]["answer"],
-    'date_from': jotform["53"]["answer"]["month"]+ ' ' + jotform["53"]["answer"]["day"],
-	'date_to': jotform["54"]["answer"]["month"]+ ' ' + jotform["54"]["answer"]["day"],
-    'numPassengers': int(jotform['50']['answer']),
-    'gender': jotform['3']['answer']
-
+	'first': jotform['5']['answer']['first'],
+	'birthday': jotform['6']['prettyFormat'],
+    'budget': int(jotform['37']['answer']),
+    'visited': jotform['38']['answer'],
+    'fly_from': jotform["34"]["answer"],
+    'date_from': jotform["35"]["answer"]["month"]+ ' ' + jotform["35"]["answer"]["day"],
+	'date_to': jotform["36"]["answer"]["month"]+ ' ' + jotform["36"]["answer"]["day"],
+    'numPassengers': int(jotform['3']['answer']),
+    'gender': jotform['7']['answer']
 }
 
-
 system_intel = 'You are a experienced travel agent. You are going to recommend one travel destination for a client based on their profile. The client wants to travel spontaneously and wants you to recommend a random destination. Make sure the destination you choose is feasible given the client’s budget and availability. For example, if a client only has 5 days to travel, you wouldn’t want to recommend a destination in Asia because the client will need more time to visit most countries in Asia. DO NOT recommend anywhere in Mexico, Canada, the United Kingdom, the Caribbean. DO NOT recommend any of the countries the client has already been to. DO NOT recommend the most visited mainstream European cities. DO recommend destinations that the average US traveler hasn’t been to. For destination recommendation, only output the country name, nothing else.  For itinerary generation, make each day sound exciting!'
-prompt = f"Your client is {query['first']}. {query['first']} is a {query['gender']} from the United States of America. He is going to be traveling with {query['numPassengers']} other person and they are flying out of {query['fly_from']}. He is leaving on {query['date_from']} and returns on {query['date_to']}. The budget for the whole trip is {query['budget']}. They have been to these countries before: {query['visited']}. The reason they travels is because they love the feeling of exploring a new place. Where should they go?"
+prompt = f"Your client is {query['first']}. {query['first']} is a {query['gender']} from the United States of America. He is going to be traveling with {query['numPassengers']} other people and they are flying out of {query['fly_from']}. He is leaving on {query['date_from']} and returns on {query['date_to']}. The budget for the whole trip is {query['budget']}. They have been to these countries before: {query['visited']}. The reason they travel is because they love the feeling of exploring a new place. Where should they go?"
 prompt2 = "Create an itinerary!"
 messages = [
     	{"role": "system", "content": system_intel}, 
@@ -55,5 +51,12 @@ response = openai.ChatCompletion.create(
     )
 itinerary = response['choices'][0]['message']['content']
 
-print(country)
-print(itinerary)
+result ={"country": country, "itinerary": itinerary}
+
+# Write to itinerary.json
+with open('data/itinerary.json', 'w') as file:
+	# Save dictionary into this file
+	json.dump(result, file, indent = 4)
+
+
+
