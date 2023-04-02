189ae6942bf7a2f3a9610063534b2e025d1e5868
Andrew Vittiglio <andrew@andrews-air.dhcp.nd.edu>
2023-04-01 22:55:38 -0400

    automatic email fixed - not great formatting

--- model/sendgridEmail.py
+++ model/sendgridEmail.py
@@ -4,6 +4,7 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from dotenv import load_dotenv
+import json
 
 load_dotenv() # Load environment variables from .env file
 # Open the JSON file for reading
@@ -11,11 +12,23 @@ with open('../view/pages/email.html', 'r') as file:
     # Load the contents of the file into a variable
     html = file.read()
 
+with open('data/itinerary.json', 'r') as file:
+    data = json.load(file)
+
+html = html.replace('{{{image1}}}', data['photos'][0])
+html = html.replace('{{{image2}}}', data['photos'][1])
+html = html.replace('{{{image3}}}', data['photos'][2])
+html = html.replace('{{{image4}}}', data['photos'][3])
+html = html.replace('---mainCountry---', data['country'])
+html = html.replace('---description---', data['description'])
+html = html.replace('---welcomeCountry---', f"Welcome to {data['country']}!")
+html = html.replace('---itinerary---', data['itinerary'])
+html = html.replace('{{{lonelyPlanet}}}', "https://www.lonelyplanet.com/" + data['country'].lower())
 
 message = Mail(
     from_email='andrewv433@gmail.com',
     to_emails='avittig2@nd.edu',
-    subject='The Wander Team Invites You To Explore!',
+    subject='Your trip is booked!',
     html_content=html)
 try:
     sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
