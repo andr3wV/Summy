733dd76a920bcbe5921d8b4241fa1d796ea2b2b5
Andrew Vittiglio <andrew@andrews-air.dhcp.nd.edu>
2023-05-01 16:03:59 -0400

    remove logs, add error message

--- api/summarize.js
+++ api/summarize.js
@@ -18,14 +18,12 @@ module.exports = async (req, res) => {
         { role: 'user', content: text },
       ],
     };
-    console.log("KEY1 -----------> " + process.env.OPENAI_API_KEY);
     const headers = {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
     };
 
     try {
-      console.log("KEY2 -----------> " + process.env.OPENAI_API_KEY);
       const response = await axios.post(
         'https://api.openai.com/v1/chat/completions',
         data,
@@ -34,6 +32,7 @@ module.exports = async (req, res) => {
 
       res.status(200).json({ summary: response.data.choices[0].message.content });
     } catch (error) {
+        console.log(error);
       res.status(500).json({ message: 'Error summarizing text. Please try again later.2' });
     }
   } else {
