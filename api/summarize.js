677d4a47bac572e8adcd6dfcee579f35bbb8b146
Andrew Vittiglio <andrew@andrews-air.dhcp.nd.edu>
2023-05-01 21:39:02 -0400

    reduce timeout with Promise.all()

--- api/summarize.js
+++ api/summarize.js
@@ -4,28 +4,28 @@ module.exports = async (req, res) => {
   var textArray = [];
   if (req.method === 'POST') {
     var fullText = req.body.text;
-    
+
     if (!fullText) {
       res.status(400).json({ message: 'No text provided.' });
       return;
     }
-    
+
     // Splice text into an object for OpenAI - every new line is a new index in an array
     fullText = fullText.split(/\n+/);
-    for(let i = 0; i < fullText.length; i++) {
-      var text = fullText[i];
+
+    const textPromises = fullText.map(async (text, i) => {
       const data = {
-          "model": "gpt-3.5-turbo",
-          "messages": [
-              {
-                  "role": "system",
-                  "content": "You are an AI that summarizes text. Summarize all of the following text"
-              },
-              {
-                  "role": "user", 
-                  "content": text
-              }
-          ]
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {
+            "role": "system",
+            "content": "You are an AI that summarizes text. Summarize all of the following text"
+          },
+          {
+            "role": "user",
+            "content": text
+          }
+        ]
       };
 
       const headers = {
@@ -37,15 +37,24 @@ module.exports = async (req, res) => {
         const response = await axios.post(
           'https://api.openai.com/v1/chat/completions',
           data,
-          { headers, timeout: 60000}
+          { headers, timeout: 60000 } // Set the timeout to 60 seconds (60000 milliseconds)
         );
-          textArray.push(response.data.choices[0].message.content);
+        return response;
       } catch (error) {
         console.error('Error:', error.message);
         res.status(500).json({ message: 'Error summarizing text. Please try again later.', error: error.message });
       }
+    });
+
+    try {
+      const results = await Promise.all(textPromises);
+      textArray = results.map(result => result.data.choices[0].message.content);
+      res.status(200).json({ summary: textArray.join('\n') });
+    } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ message: 'Error summarizing text. Please try again later.', error: error.message });
     }
-    res.status(200).json({ summary: textArray.join('\n') });
+
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
