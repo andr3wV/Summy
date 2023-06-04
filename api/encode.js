8b3afa1e6b9f366e2374024494ef21dacf20be93
Andrew Vittiglio <andrew@Andrews-MacBook-Air.local>
2023-06-03 20:06:49 -0400

    encoder for tokenization

---
+++ api/encode.js
@@ -0,0 +1,20 @@
+const tokenizer = require('gpt-tokenizer');
+
+module.exports = async (req, res) => {
+  const fullText = req.body.text;
+
+  // Encodes text to array of tokens
+  const tokens = await tokenizer.encode(fullText);
+
+  // Decodes tokens to text in chunks of 500 tokens
+  const chunks = [];
+   
+  for (let i =0; i < tokens.length; i+=500) {
+    const chunk = tokens.slice(i, i + 500);
+    const decodedChunk = await tokenizer.decode(chunk);
+    chunks.push(decodedChunk);
+  }
+
+  res.status(200).json({  summary: chunks });
+
+};
